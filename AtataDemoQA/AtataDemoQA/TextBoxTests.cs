07bd25efb5404726d6fc8978acfcff749ed7e5d5
AnnaKanunnikova-QA <avkanunnikova@gmail.com>
2024-11-01 22:44:47 +0000

    added hw 16

--- AtataDemoQA/AtataDemoQA/TextBoxTests.cs
+++ AtataDemoQA/AtataDemoQA/TextBoxTests.cs
@@ -1,9 +1,25 @@
-﻿using Atata;
-using _ = AtataDemoQA.TextBoxTests;
-
-namespace AtataDemoQA
+﻿namespace AtataDemoQA
 {
-    public sealed class TextBoxTests : Page<_>
+    public sealed class TextBoxTests : UITestFixture
     {
+        [Test]
+        public void CreateSubmitFormTest()
+        {
+            Go.To<TextBoxPage>()
+                .PageTitle.Should.Contain("DEMOQA")
+                .UserName.Type("Anna Kanunnikova")
+                .UserEmail.Type("test@test.com")
+                .CurrentAddress.Type("Portugal")
+                .PermanentAddress.Type("USA")
+                .ScrollDown()
+                .Submit.Click()
+                .OutputName.Should.Be("Name:Anna Kanunnikova")
+                .OutputEmail.Should.Be("Email:test@test.com")
+                .OutputCurrentAddress.Should.Be("Current Address :Portugal")
+                .OutputPermanentAddress.Should.Be("Permananet Address :USA");
+
+        }
     }
 }
+
+
